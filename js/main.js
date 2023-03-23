a25c919b145cf15925f97ac78ca43ce2bc8caa78
dafe2201 <Daniel_fellden@hotmail.com>
2023-03-23 21:32:20 +0100

    Changed input type to number to allow for easier use on mobile.
    Removed rounding to integers and changed it into one decimal.

--- js/main.js
+++ js/main.js
@@ -1,65 +1,79 @@
-document.getElementById("reset-btn").addEventListener("click", function() {
-    location.reload();
+document.getElementById("reset-btn").addEventListener("click", function () {
+  location.reload();
 });
 
+document
+  .getElementById("1rm-run-btn")
+  .addEventListener("click", calculateFromOneRm);
+document
+  .getElementById("eight-rep-run-btn")
+  .addEventListener("click", calculateFromEightRep);
+document
+  .getElementById("one-rep-run-btn")
+  .addEventListener("click", calculateFromOneRep);
+document
+  .getElementById("seven-rep-run-btn")
+  .addEventListener("click", calculateFromSevenRep);
+document
+  .getElementById("two-rep-run-btn")
+  .addEventListener("click", calculateFromTwoRep);
+document
+  .getElementById("six-rep-run-btn")
+  .addEventListener("click", calculateFromSixRep);
+document
+  .getElementById("three-rep-run-btn")
+  .addEventListener("click", calculateFromThreeRep);
+document
+  .getElementById("five-rep-run-btn")
+  .addEventListener("click", calculateFromFiveRep);
+document
+  .getElementById("four-rep-run-btn")
+  .addEventListener("click", calculateFromFourRep);
 
-document.getElementById("1rm-run-btn").addEventListener("click",calculateFromOneRm);
-document.getElementById("eight-rep-run-btn").addEventListener("click",calculateFromEightRep);
-document.getElementById("one-rep-run-btn").addEventListener("click",calculateFromOneRep);
-document.getElementById("seven-rep-run-btn").addEventListener("click",calculateFromSevenRep);
-document.getElementById("two-rep-run-btn").addEventListener("click",calculateFromTwoRep);
-document.getElementById("six-rep-run-btn").addEventListener("click",calculateFromSixRep);
-document.getElementById("three-rep-run-btn").addEventListener("click",calculateFromThreeRep);
-document.getElementById("five-rep-run-btn").addEventListener("click",calculateFromFiveRep);
-document.getElementById("four-rep-run-btn").addEventListener("click",calculateFromFourRep);
-
-
-function calculateFromOneRm(){
-    bulgarianFormulae(Math.round(document.getElementById("1rm").value));
+function calculateFromOneRm() {
+  bulgarianFormulae(parseFloat(document.getElementById("1rm").value));
 }
 
-function calculateFromEightRep(){
-    bulgarianFormulae(Math.round(document.getElementById("eight-rep").value / 0.6));
+function calculateFromEightRep() {
+  bulgarianFormulae(document.getElementById("eight-rep").value / 0.6);
 }
 
-function calculateFromOneRep(){
-    bulgarianFormulae(Math.round(document.getElementById("one-rep").value / 0.95));
+function calculateFromOneRep() {
+  bulgarianFormulae(document.getElementById("one-rep").value / 0.95);
 }
 
-function calculateFromSevenRep(){
-    bulgarianFormulae(Math.round(document.getElementById("seven-rep").value / 0.65));
+function calculateFromSevenRep() {
+  bulgarianFormulae(document.getElementById("seven-rep").value / 0.65);
 }
 
-function calculateFromTwoRep(){
-    bulgarianFormulae(Math.round(document.getElementById("two-rep").value / 0.90));
+function calculateFromTwoRep() {
+  bulgarianFormulae(document.getElementById("two-rep").value / 0.9);
 }
 
-function calculateFromSixRep(){
-    bulgarianFormulae(Math.round(document.getElementById("six-rep").value / 0.70));
+function calculateFromSixRep() {
+  bulgarianFormulae(document.getElementById("six-rep").value / 0.7);
 }
 
-function calculateFromThreeRep(){
-    bulgarianFormulae(Math.round(document.getElementById("three-rep").value / 0.85));
+function calculateFromThreeRep() {
+  bulgarianFormulae(document.getElementById("three-rep").value / 0.85);
 }
 
-function calculateFromFiveRep(){
-    bulgarianFormulae(Math.round(document.getElementById("five-rep").value / 0.75));
+function calculateFromFiveRep() {
+  bulgarianFormulae(document.getElementById("five-rep").value / 0.75);
 }
 
-function calculateFromFourRep(){
-    bulgarianFormulae(Math.round(document.getElementById("four-rep").value / 0.80));
+function calculateFromFourRep() {
+  bulgarianFormulae(document.getElementById("four-rep").value / 0.8);
 }
 
-
-
-function bulgarianFormulae(oneRm){
-    document.getElementById("1rm").value = oneRm;
-    document.getElementById("eight-rep").value = Math.round(oneRm * 0.60);
-    document.getElementById("one-rep").value = Math.round(oneRm * 0.95);
-    document.getElementById("seven-rep").value = Math.round(oneRm * 0.65);
-    document.getElementById("two-rep").value = Math.round(oneRm * 0.90);
-    document.getElementById("six-rep").value = Math.round(oneRm * 0.70);
-    document.getElementById("three-rep").value = Math.round(oneRm * 0.85);
-    document.getElementById("five-rep").value = Math.round(oneRm * 0.75);
-    document.getElementById("four-rep").value = Math.round(oneRm * 0.80);
+function bulgarianFormulae(oneRm) {
+  document.getElementById("1rm").value = oneRm.toFixed(1);
+  document.getElementById("eight-rep").value = (oneRm * 0.6).toFixed(1);
+  document.getElementById("one-rep").value = (oneRm * 0.95).toFixed(1);
+  document.getElementById("seven-rep").value = (oneRm * 0.65).toFixed(1);
+  document.getElementById("two-rep").value = (oneRm * 0.9).toFixed(1);
+  document.getElementById("six-rep").value = (oneRm * 0.7).toFixed(1);
+  document.getElementById("three-rep").value = (oneRm * 0.85).toFixed(1);
+  document.getElementById("five-rep").value = (oneRm * 0.75).toFixed(1);
+  document.getElementById("four-rep").value = (oneRm * 0.8).toFixed(1);
 }
